2ea5730b08d7f4ba608ab10f36c9fbef4b43c70e
Areesh18 <areeshzafar396@gmail.com>
2026-04-17 22:28:08 +0530

    make multiple circles

---
+++ 04_multiple circles.glsl
@@ -0,0 +1,20 @@
+float circle(vec2 uv,vec2 p, float r, float blur){
+    float d=length(uv-p);
+    float c=smoothstep(r,r-blur,d);
+    
+    return c;
+}
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    
+    vec2 uv = fragCoord/iResolution.xy;
+    uv=uv-0.5;
+    uv.x*=iResolution.x/iResolution.y;
+       
+    float c=circle(uv,vec2(0.0,0.0), 0.1, 0.01);
+    c+=circle(uv,vec2(-0.25,0.0),0.1,0.01);
+    c+=circle(uv,vec2(0.25,0.0),0.1,0.01);
+    c+=circle(uv,vec2(0.0,0.25),0.1,0.01);
+    c+=circle(uv,vec2(0.0,-0.25),0.1,0.01);
+    fragColor=vec4(vec3(c),1.0);
+}
