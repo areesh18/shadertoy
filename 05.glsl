2c8eb6749504584f11e622423ad7d7747e2f718c
Areesh18 <areeshzafar396@gmail.com>
2026-04-17 22:30:49 +0530

    making eyes by masking(subtracting)

---
+++ 05.glsl
@@ -0,0 +1,19 @@
+float circle(vec2 uv,vec2 p, float r, float blur){
+    float d=length(uv-p);
+    float c=smoothstep(r,r-blur,d);
+    
+    return c;
+}
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    
+    vec2 uv = fragCoord/iResolution.xy;
+    uv=uv-0.5;
+    uv.x*=iResolution.x/iResolution.y;
+       
+    float c=circle(uv,vec2(0.0,0.0), 0.4, 0.01);
+    c-=circle(uv,vec2(-0.13,0.2), 0.07, 0.01);
+    c-=circle(uv,vec2(0.13,0.2), 0.07, 0.01);
+    
+    fragColor=vec4(vec3(c),1.0);
+}
