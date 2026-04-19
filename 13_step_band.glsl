fd0c04986dfd700c8e53aeff8bf6ad12c43a3426
Areesh18 <areeshzafar396@gmail.com>
2026-04-19 23:19:23 +0530

    left right naming corrected

--- 13_step_band.glsl
+++ 13_step_band.glsl
@@ -1,8 +1,8 @@
 void mainImage(out vec4 fragColor, in vec2 fragCoord){
     vec2 uv=fragCoord/iResolution.xy;
     uv-=0.5;
-    float leftEdge=step(uv.x,0.2);
-    float rightEdge=step(-0.2,uv.x);
+    float leftEdge=step(-0.2,uv.x);
+    float rightEdge=step(uv.x,0.2);
     float mask=leftEdge*rightEdge;
     fragColor=vec4(vec3(mask),1.0);
 }
